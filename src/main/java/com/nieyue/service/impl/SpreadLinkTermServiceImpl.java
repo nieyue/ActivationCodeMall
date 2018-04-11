2c60917e805e755e250994f7a5f148cf7294e900
nieyue <278076304@qq.com>
2018-04-11 19:54:04 +0800

    2018 4 11

--- src/main/java/com/nieyue/service/impl/SpreadLinkTermServiceImpl.java
+++ src/main/java/com/nieyue/service/impl/SpreadLinkTermServiceImpl.java
@@ -9,30 +9,96 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.nieyue.bean.Account;
+import com.nieyue.bean.SpreadLink;
 import com.nieyue.bean.SpreadLinkTerm;
 import com.nieyue.dao.SpreadLinkTermDao;
+import com.nieyue.service.AccountService;
+import com.nieyue.service.SpreadLinkService;
 import com.nieyue.service.SpreadLinkTermService;
 @Service
 public class SpreadLinkTermServiceImpl implements SpreadLinkTermService{
 	@Resource
 	SpreadLinkTermDao spreadLinkTermDao;
+	@Resource
+	SpreadLinkService spreadLinkService;
+	@Resource
+	AccountService accountService;
 	@Transactional(propagation=Propagation.REQUIRED)
 	@Override
 	public boolean addSpreadLinkTerm(SpreadLinkTerm spreadLinkTerm) {
+		boolean b=false;
 		spreadLinkTerm.setCreateDate(new Date());
-		boolean b = spreadLinkTermDao.addSpreadLinkTerm(spreadLinkTerm);
+		b = spreadLinkTermDao.addSpreadLinkTerm(spreadLinkTerm);
+		if(b){//系统通知
+			//通知到所有人
+			new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					List<Account> al = accountService.browsePagingAccount(null, null, null, null, null, null, null, null, null, 1, Integer.MAX_VALUE, "account_id", "asc");
+					al.forEach((account)->{
+						Integer aid = account.getAccountId();
+						SpreadLink sl=new SpreadLink();
+						sl.setLink(spreadLinkTerm.getLink());
+						sl.setSpreadNumber(0);
+						sl.setAccountId(aid);
+						spreadLinkService.addSpreadLink(sl);
+					});
+				}
+			}).start();
+		}
 		return b;
 	}
 	@Transactional(propagation=Propagation.REQUIRED)
 	@Override
 	public boolean delSpreadLinkTerm(Integer spreadLinkTermId) {
 		boolean b = spreadLinkTermDao.delSpreadLinkTerm(spreadLinkTermId);
+		if(b){
+			//通知到所有人
+			new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					List<Account> al = accountService.browsePagingAccount(null, null, null, null, null, null, null, null, null, 1, Integer.MAX_VALUE, "account_id", "asc");
+					al.forEach((account)->{
+						Integer aid = account.getAccountId();
+						List<SpreadLink> sll = spreadLinkService.browsePagingSpreadLink(aid, 1, Integer.MAX_VALUE, "spread_link_id", "asc");
+						if(sll.size()==1){
+						SpreadLink spreadLink=sll.get(0);
+						spreadLinkService.delSpreadLink(spreadLink.getSpreadLinkId());
+						}
+					});
+				}
+			}).start();
+		}
 		return b;
 	}
 	@Transactional(propagation=Propagation.REQUIRED)
 	@Override
 	public boolean updateSpreadLinkTerm(SpreadLinkTerm spreadLinkTerm) {
 		boolean b = spreadLinkTermDao.updateSpreadLinkTerm(spreadLinkTerm);
+		if(b){
+			//通知到所有人
+			new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					List<Account> al = accountService.browsePagingAccount(null, null, null, null, null, null, null, null, null, 1, Integer.MAX_VALUE, "account_id", "asc");
+					al.forEach((account)->{
+						Integer aid = account.getAccountId();
+						List<SpreadLink> sll = spreadLinkService.browsePagingSpreadLink(aid, 1, Integer.MAX_VALUE, "spread_link_id", "asc");
+						if(sll.size()==1){
+							SpreadLink sl=sll.get(0);
+							sl.setLink(spreadLinkTerm.getLink());
+							sl.setSpreadNumber(sl.getSpreadNumber());
+							sl.setAccountId(aid);
+							spreadLinkService.updateSpreadLink(sl);
+						}
+					});
+				}
+			}).start();
+		}
 		return b;
 	}
 
